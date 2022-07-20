25e211bfe195fa43be44b3e21fb9c4833e4e1a40
Farahalhalabi <farahalhalabi11@gmail.com>
2022-07-20 14:33:02 -0500

    upload my code

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@ const myfunc = $(document).ready(function () {
     var check=true
     $(".mySkills").mouseenter(function () {
         if (check === true) {
-            alert("Welcome to Farah's Portfolio!");
+            //alert("Welcome to Farah's Portfolio!");
             check=false
 
         }
