f158cd715456ae4f094999846d9a78e9910b5c23
Farahalhalabi <farahalhalabi11@gmail.com>
2022-07-23 11:57:39 -0500

    updated homework!

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 const myfunc = $(document).ready(function () {
     var check=true
-    $(".mySkills").mouseenter(function () {
+    $(".alert-box").mouseenter(function () {
         if (check === true) {
             alert("Welcome to Farah's Portfolio!");
             check=false
